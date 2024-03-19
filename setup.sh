e18acd19ac468e44c268c7cc4d9e3530e460440f
VissaMoutafis <vissarionm2000@gmail.com>
2024-03-19 01:02:20 +0100

    made setup script for iptables, suricata installation and docker compose - nice todo: make it a service

---
+++ setup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+setup_suricata() {
+    config_yaml="$1"
+
+    echo "Installing Suricata IDS..."
+    echo | sudo add-apt-repository ppa:oisf/suricata-stable
+    sudo apt install suricata jq -y
+
+    # copy your configurations in /etc/suricata/suricata.yaml
+    echo "Copying configurations to /etc/suricata/suricata.yaml..."
+    sudo cp ./suricata-config/suricata.yaml /etc/suricata/suricata.yaml
+
+    # copy your rules in /etc/suricata/rules
+    echo "Copying rules to /etc/suricata/rules..."
+    sudo cp -r ./suricata-config/rules /etc/suricata/
+
+    # echo "Setting up suricata service..."
+    # # copy the IDS service file into /etc/systemd/system
+    # sudo cp suricata.service /etc/systemd/system/suricata.service
+
+    # # reload the systemd daemon
+    # sudo systemctl daemon-reload
+
+    # echo "Starting/Enabling Suricata service..."
+    # # # start the suricata engine and make sure it starts on boot
+    # sudo systemctl start suricata.service
+    # sudo systemctl enable suricata.service
+
+    # sudo /usr/bin/suricata -q 0 -c /etc/suricata/suricata.yaml --pidfile /var/run/suricata.pid -D
+    echo "Run 'sudo /usr/bin/suricata -q 0 -c /etc/suricata/suricata.yaml -D --pidfile /var/run/suricata.pid' in case you want to start suricata..." 
+    # set up iptables for interface br-android to forward in NFQUEUE
+    echo "Setting up iptables for interface br-android..."
+    sudo iptables -I INPUT -i br-android -j NFQUEUE --queue-num 0
+    sudo iptables -I OUTPUT -o br-android -j NFQUEUE --queue-num 0
+}
+
+
+start_docker() {
+    echo "Starting docker network and containers..."
+    docker compose up -d --build
+}
+
+start_docker
+setup_suricata $1
+
