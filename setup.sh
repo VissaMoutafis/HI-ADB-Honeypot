a94d594e8f7d9a6483edc0019f056aa259cbc491
ElKozel <caci802@gmail.com>
2024-03-22 16:05:02 +0100

    small change

--- setup.sh
+++ setup.sh
@@ -26,6 +26,7 @@ setup_docker() {
         echo "docker group does not exist... creating docker group."
         sudo groupadd docker
     fi
+
     sudo usermod -aG docker $USER
     newgrp docker
 }
@@ -43,14 +44,10 @@ setup_suricata() {
     echo "Copying rules to /etc/suricata/rules..."
     sudo cp -r ./suricata-config/rules /etc/suricata/
 
-    # create suri:suri user and group
-    echo "Creating suri:suri user and group..."
-    sudo useradd -r -s /usr/sbin/nologin suri
-    sudo groupadd suri
-    sudo usermod -a -G suri suri
-
-    sudo chown root:suri /var/log/suricata
-    sudo chmod 750 /var/log/suricata
+    # set up iptables for interface br-android to forward in NFQUEUE
+    echo "Setting up iptables for interface br-android..."
+    sudo iptables -I INPUT -i br-android -j NFQUEUE --queue-num 0
+    sudo iptables -I OUTPUT -o br-android -j NFQUEUE --queue-num 0
 
     # # copy the IDS service file into /etc/systemd/system
     echo "Setting up suricata service..."
@@ -64,26 +61,26 @@ setup_suricata() {
     sudo systemctl start suricata.service
     sudo systemctl enable suricata.service
 
+    sudo systemctl restart suricata.service
+
     # sudo /usr/bin/suricata -q 0 -c /etc/suricata/suricata.yaml --pidfile /var/run/suricata.pid -D
     # echo "Run 'sudo /usr/bin/suricata -q 0 -c /etc/suricata/suricata.yaml -D --pidfile /var/run/suricata.pid' in case you want to start suricata..." 
-    # set up iptables for interface br-android to forward in NFQUEUE
-    echo "Setting up iptables for interface br-android..."
-    sudo iptables -I INPUT -i br-android -j NFQUEUE --queue-num 0
-    sudo iptables -I OUTPUT -o br-android -j NFQUEUE --queue-num 0
+    
 }
 
 
 start_docker() {
+    # check if docker and docker-compose is installed
+    if ! [ -x "$(command -v docker)" ]; then
+        echo "Docker is not installed. Installing Docker..."
+        setup_docker
+    fi
+
     echo "Starting docker network and containers..."
     docker compose up -d --build
 }
 
 
-# check if docker and docker-compose is installed
-if ! [ -x "$(command -v docker)" ]; then
-    echo "Docker is not installed. Installing Docker..."
-    setup_docker
-fi
 start_docker
 setup_suricata
 
