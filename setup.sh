91c2e70f7d7ec5240693303210c2d18644b244e4
VissaMoutafis <vissarionm2000@gmail.com>
2024-03-22 14:33:30 +0100

    semi-ready

--- setup.sh
+++ setup.sh
@@ -15,20 +15,29 @@ setup_suricata() {
     echo "Copying rules to /etc/suricata/rules..."
     sudo cp -r ./suricata-config/rules /etc/suricata/
 
-    # echo "Setting up suricata service..."
+    # create suri:suri user and group
+    echo "Creating suri:suri user and group..."
+    sudo useradd -r -s /usr/sbin/nologin suri
+    sudo groupadd suri
+    sudo usermod -a -G suri suri
+
+    sudo chown root:suri /var/log/suricata
+    sudo chmod 750 /var/log/suricata
+
     # # copy the IDS service file into /etc/systemd/system
-    # sudo cp suricata.service /etc/systemd/system/suricata.service
+    echo "Setting up suricata service..."
+    sudo cp suricata.service /etc/systemd/system/suricata.service
 
     # # reload the systemd daemon
-    # sudo systemctl daemon-reload
+    sudo systemctl daemon-reload
 
     # echo "Starting/Enabling Suricata service..."
     # # # start the suricata engine and make sure it starts on boot
-    # sudo systemctl start suricata.service
-    # sudo systemctl enable suricata.service
+    sudo systemctl start suricata.service
+    sudo systemctl enable suricata.service
 
     # sudo /usr/bin/suricata -q 0 -c /etc/suricata/suricata.yaml --pidfile /var/run/suricata.pid -D
-    echo "Run 'sudo /usr/bin/suricata -q 0 -c /etc/suricata/suricata.yaml -D --pidfile /var/run/suricata.pid' in case you want to start suricata..." 
+    # echo "Run 'sudo /usr/bin/suricata -q 0 -c /etc/suricata/suricata.yaml -D --pidfile /var/run/suricata.pid' in case you want to start suricata..." 
     # set up iptables for interface br-android to forward in NFQUEUE
     echo "Setting up iptables for interface br-android..."
     sudo iptables -I INPUT -i br-android -j NFQUEUE --queue-num 0
